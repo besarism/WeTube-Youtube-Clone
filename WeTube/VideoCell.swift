5aa1a013b0c8376196789ef8a333fc15c72e0d9b
besarism <b3sar11@gmail.com>
2017-12-22 22:06:54 +0100

    Views added to VideoCell including constraints with different ways of implementing them

--- WeTube/VideoCell.swift
+++ WeTube/VideoCell.swift
@@ -8,15 +8,145 @@
 
 import UIKit
 
-class VideoCell: UICollectionViewCell {
+
+class VideoCell: BaseCell {
+    
+    var thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .blue
+        
+        return imageView
+    }()
+    
+    var titleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .purple
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    var descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .red
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textView
+    }()
+    
+    var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .green
+        
+        return imageView
+    }()
+    
+    let separatorLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        
+        return  view
+    }()
+    
+    
+    override func setupViews() {
+        //add subviews
+        addSubview(thumbnailImageView)
+        addSubview(separatorLine)
+        addSubview(profileImageView)
+        addSubview(titleLabel)
+        addSubview(descriptionTextView)
+        
+        
+        //constraints for the subviews
+        addConstraintsWithFormat(format: "H:|-16-[v0]-16-|", views: thumbnailImageView)
+        addConstraintsWithFormat(format: "H:|-16-[v0(44)]", views: profileImageView)
+        addConstraintsWithFormat(format: "H:|[v0]|", views: separatorLine)
+        addConstraintsWithFormat(format: "V:|-16-[v0]-8-[v1(44)]-16-[v2(1)]|", views: thumbnailImageView, profileImageView, separatorLine)
+
+        //top constraint for the titlelabel
+        //v1:
+//        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: thumbnailImageView, attribute: .bottom, multiplier: 1, constant: 8))
+        //v2:
+        titleLabel.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 8).isActive = true
+        
+        
+        //left constraint for the title label
+        //v1:
+//        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .left, relatedBy: .equal, toItem: profileImageView, attribute: .right, multiplier: 1, constant: 8))
+        //v2:
+        titleLabel.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 8).isActive = true
+
+//        //right constraint for the title label
+//        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
+        //v2:
+        titleLabel.rightAnchor.constraint(equalTo: thumbnailImageView.rightAnchor, constant: 0).isActive = true
+        
+        //height constraint for the title label
+        //v1:
+        //        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+        //v2:
+        titleLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        
+        
+        
+        
+        //top constraint for the description textView
+        addConstraint(NSLayoutConstraint(item: descriptionTextView, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1, constant: 4))
+        
+        
+        //left constraint for the description textView
+        addConstraint(NSLayoutConstraint(item: descriptionTextView, attribute: .left, relatedBy: .equal, toItem: profileImageView, attribute: .right, multiplier: 1, constant: 8))
+        
+        //        //right constraint for the description textView
+        addConstraint(NSLayoutConstraint(item: descriptionTextView, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
+        
+        
+        
+        //height constraint for the description textView
+        addConstraint(NSLayoutConstraint(item: descriptionTextView, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+       
+        
+        
+    }
+    
+}
+
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (key, view) in views.enumerated() {
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary["v\(key)"] = view
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat:format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+class BaseCell: UICollectionViewCell {
+    
+    func setupViews() {
+        
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        setupViews()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
