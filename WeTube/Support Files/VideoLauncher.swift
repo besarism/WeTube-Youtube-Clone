91281feb6b496e9622f02e994dca1bb93e475824
besarism <b3sar11@gmail.com>
2018-05-01 18:39:17 +0200

    Video slider added

--- WeTube/Support Files/VideoLauncher.swift
+++ WeTube/Support Files/VideoLauncher.swift
@@ -10,18 +10,151 @@ import UIKit
 import AVFoundation
 
 class VideoPlayerView: UIView {
+ 
+    
+    //Vars
+    let controlsView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 1)
+        
+        return view
+    }()
+    
+    var player: AVPlayer?
+    
+    var isPlaying = false
+    
+    let videoLengthLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .right
+        label.text = "00:00"
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+//        label.backgroundColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    let videoSlider: UISlider = {
+        let slider = UISlider()
+        slider.minimumTrackTintColor = .red
+        slider.maximumTrackTintColor = .white
+        slider.setThumbImage(UIImage(named: "thumb"), for: .normal)
+        slider.addTarget(self, action: #selector(handleSlider), for: .valueChanged)
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        
+        return slider
+    }()
+    
+    let activityIndicatorView: UIActivityIndicatorView = {
+        let aiv = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        aiv.startAnimating()
+        aiv.translatesAutoresizingMaskIntoConstraints = false
+        
+        return aiv
+    }()
+    
+    lazy var ppButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "pause")
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(handlePP), for: .touchUpInside)
+        button.isHidden = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupPlayerView()
+        
+        controlsView.frame = self.frame
+        addSubview(controlsView)
+        controlsView.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        controlsView.addSubview(ppButton)
+        ppButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        ppButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        ppButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        ppButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        controlsView.addSubview(videoLengthLabel)
+        videoLengthLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -8).isActive = true
+        videoLengthLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        videoLengthLabel.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        videoLengthLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        controlsView.addSubview(videoSlider)
+        videoSlider.rightAnchor.constraint(equalTo: videoLengthLabel.leftAnchor).isActive = true
+        videoSlider.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        videoSlider.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        videoSlider.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        
         backgroundColor = .black
         
+        
+    }
+    
+    
+    func setupPlayerView() {
         let urlString = "http://ibelian.com/Services/Videos/Norwegian%20Verbs%20app.mov"
         if let url = URL(string: urlString) {
-            let player = AVPlayer(url: url)
+            player = AVPlayer(url: url)
             let playerLayer = AVPlayerLayer(player: player)
             self.layer.addSublayer(playerLayer)
             playerLayer.frame = self.frame
             
-            player.play()
+            player?.play()
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+        }
+
+    }
+    
+    @objc private func handlePP(sender: UIButton) {
+        
+        if isPlaying {
+            sender.setImage(UIImage(named: "play"), for: .normal)
+            player?.pause()
+        } else {
+            sender.setImage(UIImage(named: "pause"), for: .normal)
+            player?.play()
+        }
+        isPlaying = !isPlaying
+    }
+    
+    @objc private func handleSlider() {
+        print(videoSlider.value)
+        if let duration = player?.currentItem?.duration {
+            let seconds = CMTimeGetSeconds(duration)
+            let value = videoSlider.value * Float(seconds)
+            let seekTime = CMTime(value: CMTimeValue(value), timescale: 1)
+            player?.seek(to: seekTime, completionHandler: { (completed) in
+                // ??
+            })
+            
+        }
+        
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentItem.loadedTimeRanges" {
+            activityIndicatorView.stopAnimating()
+            controlsView.backgroundColor = .clear
+            ppButton.isHidden = false
+            isPlaying = true
+            
+            if let duration = player?.currentItem?.duration {
+                let seconds = CMTimeGetSeconds(duration)
+                let sec = Int(seconds) % 60
+                let min = String(format: "%02d", Int(seconds) / 60)
+                videoLengthLabel.text = "\(min):\(sec)"
+            }
         }
     }
     
